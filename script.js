d3c5ba2aa6fab87c15d590fccee087cd72b4db64
dennisil <dennisilye@gmail.com>
2022-12-24 00:24:32 +0200

    added input handler and player class

--- script.js
+++ script.js
@@ -1,34 +1,85 @@
-window.addEventListener('load', function () {
-    const canvas = document.getElementById('canvas1')
-    const ctx = canvas.getContext('2d')
-    canvas.width = 800
-    canvas.height = 720
+window.addEventListener("load", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 720;
 
-    class InputHandler {
+  class InputHandler {
+    constructor() {
+      this.keys = [];
+      window.addEventListener("keydown", (event) => {
+        console.log("event", event.key);
 
+        if (
+          this._checkIfArrowKey(event.key) &&
+          this.keys.indexOf(event.key) === -1
+        ) {
+          this.keys.push(event.key);
+        }
+        console.log(this.keys);
+      });
+
+      window.addEventListener("keyup", (event) => {
+        console.log("event", event.key);
+
+        if (this._checkIfArrowKey(event.key)) {
+          this.keys.splice(this.keys.indexOf(event.key, 1));
+        }
+        console.log(this.keys);
+      });
     }
 
-    class Player {
+    _checkIfArrowKey(key) {
+      return (
+        key === "ArrowDown" ||
+        key === "ArrowUp" ||
+        key === "ArrowLeft" ||
+        key === "ArrowRight"
+      );
+    }
+  }
 
+  class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = this.width - this.height;
+      this.frameX = 0
+      this.frameY = 0
+      this.image = document.getElementById("playerImage");
+      this.speed = 0
     }
 
-    class Background {
-
+    draw(context) {
+      context.fillStyle = "white";
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image,this.frameX,this.frameY,this.width,this.height,this.x,this.y,this.width,this.height)
     }
 
-    class Enemy {
-
+    update(input) {
+        this.x += this.speed
     }
+  }
 
-    function handleEnemies() {
+  class Background {}
 
-    }
+  class Enemy {}
 
-    function displayStatusText() {
+  function handleEnemies() {}
 
-    }
+  function displayStatusText() {}
 
-    function animate() {
+  const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
 
-    }
-})
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw(ctx);
+    player.update(input);
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
